929d7a60f76e2203e11ad4706be75794272ad744
JelmerS <jelmer.siljee+github@gmail.com>
2017-03-10 14:59:50 +0100

    redo filestructure

--- cisco_jinja.py
+++ cisco_jinja.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import jinja2
-
+import yaml
 
 test_dict = {"vlan":3, "hostname":"Switch"}
 
-template_directory = "./testfiles"
+template_directory = "./testfiles/templates/"
 template_file = "test_template.config"
 output_directory = ""
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_directory))
